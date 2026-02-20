fb2a44f30a348f14f2a3fd221f5f3fbe13b588ed
tve24el030-svg <tve24el030@cet.ac.in>
2026-02-20 09:53:25 +0530

    code uploaded

--- src/ProducerConsumer.java
+++ src/ProducerConsumer.java
@@ -3,40 +3,72 @@ class SharedResource {
     boolean available = false;
 
     // TODO: synchronize void put(int item)
-    // while(available) -> wait()
-    // set this.item = item, available = true
-    // print "Produced: " + item
-    // notify()
-
-    // TODO: synchronize void get()
-    // while(!available) -> wait()
-    // print "Consumed: " + item
-    // available = false
-    // notify()
+    synchronized void put(int item){
+        while(available){
+           try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        this.item=item;
+        available=true;
+        System.out.println("Produced: "+item);
+        notify();
+    }
+    
+    synchronized void get(){
+        while(!available){
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Consumed: "+item);
+        available=false;
+        notify();
+    }
 }
 
 class Producer extends Thread {
+
     SharedResource resource;
+    Producer(SharedResource obj) {
+        this.resource = obj;
+    }
     // TODO: Constructor to init resource
     
-    // TODO: run()
-    // Loop 1 to 5
-    // call resource.put(i)
+    public void run(){
+        for (int i =1; i <=5; i++) {
+            resource.put(i);
+        }
+    }
+
 }
 
 class Consumer extends Thread {
+
     SharedResource resource;
+    Consumer(SharedResource obj) {
+        this.resource = obj;
+    }
     // TODO: Constructor to init resource
-    
-    // TODO: run()
-    // Loop 1 to 5
-    // call resource.get()
+    public void run(){
+        for (int i =1; i <=5; i++) {
+            resource.get();
+        }
+    }
+
 }
 
 public class ProducerConsumer {
     public static void main(String[] args) {
         // TODO: Create SharedResource object
-        // TODO: Create Producer and Consumer threads
-        // TODO: Start both threads
+        SharedResource obj=new SharedResource();
+        Producer t1=new Producer(obj);
+        Consumer t2=new Consumer(obj);
+        t1.start();
+        t2.start();
     }
 }
